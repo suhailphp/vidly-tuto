435cd0750a32a2bd7e0b0032214aa682887afdd3
Suhail M <suhailphp@gmail.com>
2020-06-30 15:27:24 +0400

    Movies table component Class chabnged to Stateless function

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,47 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
 import Like from "./common/like";
 
-class MoviesTable extends Component {
-  render() {
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-            <th>Fav</th>
-            <th>Delete</th>
+const MoviesTable = ({ movies, onLike, onDelete }) => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Stock</th>
+          <th>Rate</th>
+          <th>Fav</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {movies.map((movie) => (
+          <tr key={movie._id}>
+            <td>{movie.title}</td>
+            <td>{movie.genre.name}</td>
+            <td>{movie.numberInStock}</td>
+            <td>{movie.dailyRentalRate}</td>
+            <td>
+              <Like liked={movie.liked} onClick={() => onLike(movie)} />
+            </td>
+            <td>
+              <button
+                onClick={() => onDelete(movie._id)}
+                className="btn btn-danger btn-small"
+              >
+                Delete
+              </button>
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {this.props.movies.map((movie) => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <Like
-                  liked={movie.liked}
-                  onClick={() => this.props.onLike(movie)}
-                />
-              </td>
-              <td>
-                <button
-                  onClick={() => this.props.onDelete(movie._id)}
-                  className="btn btn-danger btn-small"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
-}
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default MoviesTable;
