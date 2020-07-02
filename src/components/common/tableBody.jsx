b143349d674beab7866e0eb88953678befba41d4
Suhail M <suhailphp@gmail.com>
2020-07-02 16:48:48 +0400

    Genre Name issue solved using lodash (_.get(item,path))

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
+import _ from "lodash";
 
 class TableBody extends Component {
   render() {
     let { columns, data } = this.props;
     return (
       <tbody>
-        {data.map((d) => (
-          <tr key={d._id}>
+        {data.map((item) => (
+          <tr key={item._id}>
             {columns.map((column) => (
-              <td key={d._id + (column.label || column.key)}>
-                {d[column.path]}
+              <td key={item._id + (column.label || column.key)}>
+                {_.get(item, column.path)}
               </td>
             ))}
           </tr>
