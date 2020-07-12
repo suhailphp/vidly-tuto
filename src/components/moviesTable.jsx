c682d9ee300d80c0987ae1feae0328d021608e6a
suhailphp <suhailphp@gmail.com>
2020-07-12 16:55:20 +0400

    using template litrul in Movies table to put link for movie form

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -11,7 +11,7 @@ class MoviesTable extends Component {
         label: "Title",
         path: "title",
         content: (movie) => (
-          <Link to={"movies/" + movie._id}>{movie.title}</Link>
+          <Link to={`movies/${movie._id}`}>{movie.title}</Link>
         ),
       },
       { label: "Genre", path: "genre.name" },
