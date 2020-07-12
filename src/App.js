e4bdc12a6674c8bd8a4fa0b536816b7871886b3d
suhailphp <suhailphp@gmail.com>
2020-07-12 15:56:06 +0400

    Add home page and Implememnt Routing using React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Movies from "./components/movies";
+import Home from "./components/home";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <div>
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </main>
+    </div>
   );
 }
 
