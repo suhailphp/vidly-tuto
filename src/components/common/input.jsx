d5851ce0837e8d64d71af8fd726b173888e6d280
suhailphp <suhailphp@gmail.com>
2020-07-16 13:37:44 +0400

    Using Rest operator (...rest) to extract all other parameeters in input filed component.

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 
-const Input = ({ label, name, value, onChange, error, type }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor="{id}">{label}</label>
-      <input
-        className="form-control"
-        type={type}
-        name={name}
-        id={name}
-        value={value}
-        onChange={onChange}
-      />
+      <input className="form-control" name={name} id={name} {...rest} />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
 };
 
-Input.Input = {
-  type: "text",
-};
 export default Input;
