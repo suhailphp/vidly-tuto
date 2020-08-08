ccffeec1631484aeae2415bed720b5e55ed49e50
suhailphp <suhailphp@gmail.com>
2020-08-08 18:52:07 +0400

    Using jwt-decode we are decording token value and console in app.js componentDidMount Function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import Movies from "./components/movies";
@@ -12,27 +12,36 @@ import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-function App() {
-  return (
-    <div>
-      <ToastContainer />
-      <NavBar />
-      <main className="container">
-        <Switch>
-          <Route path="/login" component={LoginForm} />
-          <Route path="/register" component={RegisterForm} />
-          <Route path="/movies/:_id" component={MovieForm} />
-          <Route path="/movies" component={Movies} />
-          <Route path="/customers" component={Customers} />
-          <Route path="/rental" component={Rental} />
-          <Route path="/notFound" component={NotFound} />
-          <Route path="/" exact component={Home} />
-          <Redirect to="/notFound" />
-        </Switch>
-      </main>
-    </div>
-  );
+import jwtDecode from "jwt-decode";
+class App extends Component {
+  state = {};
+  componentDidMount() {
+    const jwt = localStorage.getItem("token");
+    let user = jwtDecode(jwt);
+    this.setState({ user });
+    console.log(user);
+  }
+  render() {
+    return (
+      <div>
+        <ToastContainer />
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/movies/:_id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rental" component={Rental} />
+            <Route path="/notFound" component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/notFound" />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
