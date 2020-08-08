60ee6c33b2746730ccd529c99d301d9ed91bc56d
suhailphp <suhailphp@gmail.com>
2020-08-08 17:06:20 +0400

    user servicde done

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,14 +2,6 @@ import http from "./httpService";
 import { ApiEndPoint } from "../config.json";
 let endPoint = ApiEndPoint + "user";
 
-export function getUsers() {
-  return http.get(endPoint);
-}
-
-export function getUser(id) {
-  return http.get(endPoint + "/" + id);
-}
-
 export function registerUser(user) {
   if (user.UserID) {
     return http.put(endPoint + "/" + user.UserID, user);
@@ -17,6 +9,14 @@ export function registerUser(user) {
   return http.post(endPoint, user);
 }
 
-export function deleteUser(id) {
-  return http.delete(endPoint + "/" + id);
-}
+// export function getUsers() {
+//   return http.get(endPoint);
+// }
+
+// export function getUser(id) {
+//   return http.get(endPoint + "/" + id);
+// }
+
+// export function deleteUser(id) {
+//   return http.delete(endPoint + "/" + id);
+// }
