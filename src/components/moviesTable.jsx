60ce15fde3cc2144b39b76a387083e2d0547406a
suhailphp <suhailphp@gmail.com>
2020-08-09 15:28:35 +0400

    Show Delete Button only if user is admin

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -2,45 +2,56 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Like from "./common/like";
 import Table from "./common/table";
+import { getCurrentUser } from "../services/authService";
 
 class MoviesTable extends Component {
+  columns = [
+    {
+      label: "Title",
+      path: "title",
+      content: (movie) => (
+        <Link to={`/movies/${movie.movieID}`}>{movie.title}</Link>
+      ),
+    },
+    { label: "Genre", path: "Genre.name" },
+    { label: "Stock", path: "numberInStock" },
+    { label: "Rate", path: "dailyRentalRate" },
+    {
+      key: "like",
+      content: (movie) => (
+        <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
+      ),
+    },
+  ];
+  deleteColumn = {
+    key: "delete",
+    content: (movie) => (
+      <button
+        onClick={() => this.props.onDelete(movie.movieID)}
+        className="btn btn-danger btn-small"
+      >
+        Delete
+      </button>
+    ),
+  };
+
+  constructor() {
+    super();
+    let user = getCurrentUser();
+    if (user && user.isAdmin) {
+      this.columns.push(this.deleteColumn);
+    }
+  }
+
   render() {
     let { movies, sortColumn, onSort } = this.props;
-    let columns = [
-      {
-        label: "Title",
-        path: "title",
-        content: (movie) => (
-          <Link to={`/movies/${movie.movieID}`}>{movie.title}</Link>
-        ),
-      },
-      { label: "Genre", path: "Genre.name" },
-      { label: "Stock", path: "numberInStock" },
-      { label: "Rate", path: "dailyRentalRate" },
-      {
-        key: "like",
-        content: (movie) => (
-          <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
-        ),
-      },
-      {
-        key: "delete",
-        content: (movie) => (
-          <button
-            onClick={() => this.props.onDelete(movie.movieID)}
-            className="btn btn-danger btn-small"
-          >
-            Delete
-          </button>
-        ),
-      },
-    ];
+
     return (
       <Table
         onSort={onSort}
         data={movies}
         sortColumn={sortColumn}
-        columns={columns}
+        columns={this.columns}
       />
     );
   }
